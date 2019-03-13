68d32a2551ebad8a34e0b395bd70fa239be711f1
datamonk <acamara86@gmail.com>
2019-03-13 13:50:20 -0400

    another script cleanup

--- scripts/get_slides.sh
+++ scripts/get_slides.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 CWD=$(/bin/pwd -P)
-OUTPUT_PREFIX="/path/to/slides/dir"
-
-# Overwrite existing content in slides directory?
-# TODO
+OUTPUT_PREFIX="/home/afcamar/proj/gitlab/strata19/slides/"
 
 # Download Oreilly Strata slidedeck web source page. Iterate
 # through to parse and download URL paths meeting the file
 # extention critera (PDF's, PPT, ZIP). Write to OUTPUT_PREFIX
 # directory.
-wget 'https://conferences.oreilly.com/strata/strata-ca/public/schedule/proceedings' && cat ${CWD}/proceedings | grep '.ppt\|.pptx\|.pdf\|.zip' | cut -d'"' -f2 | while read line; do wget "${line}" --directory-prefix=${OUTPUT_PREFIX}; done < "${1:-/dev/stdin}"
+wget 'https://conferences.oreilly.com/strata/strata-ca/public/schedule/proceedings' \
+	&& cat ${CWD}/proceedings \
+	| grep '.ppt\|.pptx\|.pdf\|.zip' \
+	| cut -d'"' -f2 \
+	| while read line; do wget "${line}" -P ${OUTPUT_PREFIX}; done < "${1:-/dev/stdin}"
 
 # Fix garbage chars and whitespace in filenames
 detox -s iso8859_1 -v ${OUTPUT_PREFIX}/*.[pz][pdi][tfp]*

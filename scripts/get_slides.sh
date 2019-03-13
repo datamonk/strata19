b6b4e82e4b274915c237946725eec650157c770c
datamonk <acamara86@gmail.com>
2019-03-13 13:42:22 -0400

    added a few tweaks to slide DL script.

--- scripts/get_slides.sh
+++ scripts/get_slides.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-wget 'https://conferences.oreilly.com/strata/strata-ca/public/schedule/proceedings' && cat proceedings | grep '.ppt\|.pptx\|.pdf\|.zip' | cut -d'"' -f2 | while read line; do wget "$line"; done < "${1:-/dev/stdin}"
+CWD=$(/bin/pwd -P)
+OUTPUT_PREFIX="/path/to/slides/dir"
+
+# Overwrite existing content in slides directory?
+# TODO
+
+# Download Oreilly Strata slidedeck web source page. Iterate
+# through to parse and download URL paths meeting the file
+# extention critera (PDF's, PPT, ZIP). Write to OUTPUT_PREFIX
+# directory.
+wget 'https://conferences.oreilly.com/strata/strata-ca/public/schedule/proceedings' && cat ${CWD}/proceedings | grep '.ppt\|.pptx\|.pdf\|.zip' | cut -d'"' -f2 | while read line; do wget "${line}" --directory-prefix=${OUTPUT_PREFIX}; done < "${1:-/dev/stdin}"
+
+# Fix garbage chars and whitespace in filenames
+detox -s iso8859_1 -v ${OUTPUT_PREFIX}/*.[pz][pdi][tfp]*
+
+# Cleanup web source file
+rm -f ${CWD}/proceedings
